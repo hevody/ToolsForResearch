655da989a886c204c974266b5c44a182c321315b
Velocity <145270377+hevody@users.noreply.github.com>
2026-07-29 17:38:59 +0800

    Add files via upload

---
+++ tagaalpabeto.py
@@ -0,0 +1,9 @@
+authors = ['Kass', 'Hardcastle', 'Briffa', 'Yildrim',
+           'Saleek', 'Smiderle', 'Karabiyik', 'Araujo',
+           'Khozaei', 'Fernandez', 'Lister', 'Koay', 'Van der Weel',
+           'Ruohomaki', 'American Heritage']
+
+authors.sort()
+
+for author in authors:
+    print(author, sep='')
